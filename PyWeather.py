f8ec224078966a2ba470572f62bdda93b1910335
Axel69007 <ingrao.axel.al@gmail.com>
2025-05-04 17:39:28 +0200

    requete APi OK, ecriture sur un json OK

--- PyWeather.py
+++ PyWeather.py
@@ -8,30 +8,43 @@ from tkinter import simpledialog
 import tkinter as tk
 import os
 import requests
+import json
 
 #Déclaration variable
+def quitter_programme():
+    root.destroy()
+
+def final_url():
+    global ville
+    def lecture_api():
+        with open("api.tmp") as fichier_api:
+            raw_api_key=str((fichier_api.read()))
+            return raw_api_key
+    api_key = lecture_api()
+    base_url="https://api.openweathermap.org/data/2.5/weather?"
+    ville_url="q="+ ville
+    units_url="&units=metric"
+    full_url= base_url + ville_url + units_url + "&appid=" + api_key
+    return full_url
+
+def resquest_weather():
+    raw_weather_data = requests.get(final_url()).json()
+    weather_data = str(raw_weather_data)
+    with open("weather.json","w") as fichier_weather:
+        json.dump(raw_weather_data, fichier_weather, indent=4, sort_keys=True)
+
 def ajout_ville():
+    global ville
     demande = simpledialog.askstring("Ajout du nom de la ville","Entrez le nom de la ville : ")
     if demande:
         ville = str(demande)
         label_nomville.config(text="Le nom de votre ville est : " + ville)
     return ville
-def final_url():
-    base_url="https://api.openweathermap.org/data/2.5/weather?"
-    ville_url="q="+ville
-    api_url="&appid="+api_key
-    full_url= str(base_url + ville_url + api_url)
-    return full_url
-def resquest_weather():
-    weather_data = requests.get(final_url).json()
-    return weather_data
-def quitter_programme():
-    root.destroy()
+
 
 
 #initialisation variable
 ville=""
-api_key="XX"
 
 root = tk.Tk()
 root.title("PyWeather")
@@ -50,7 +63,7 @@ label_nomville.pack()
 bouton_ajouter = Button(master= boite_bouton, text="Ajoutez le nom de la ville", font="Helvetica 12" ,command=ajout_ville , width = 20)
 bouton_ajouter.pack(pady= 5)
 
-bouton_voir = Button(master= boite_bouton, text="Voir la météo",font="Helvetica 12" , width = 20)
+bouton_voir = Button(master= boite_bouton, text="Voir la météo",font="Helvetica 12",command=resquest_weather , width = 20)
 bouton_voir.pack(pady= 5)
 
 bouton_quitter = Button(boite_bouton, text="Quitter programe",font="Helvetica 12" ,command=quitter_programme , width = 20)
